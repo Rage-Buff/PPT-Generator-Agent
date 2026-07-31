17f0386202ecb85725a82d87c08ef5323f2d0877
Rage-Buff <mehul222008@gmail.com>
2026-07-31 15:37:02 +0530

    Fix variable name and improve success messages

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ import numpy as np
 import streamlit as st
 
 #===============API-KEYS================
-GOOOGLE_API_KEY = st.sidebar.text_input("Google-API",type = "password")
+GOOGLE_API_KEY = st.sidebar.text_input("Google-API",type = "password")
 GROQ_API_KEY = st.sidebar.text_input("Groq-API",type = "password")
 TAVILY_API_KEY = st.sidebar.text_input("Tavily-API",type = "password")
 
@@ -20,22 +20,22 @@ os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 os.environ["TAVILY_API_KEY"] = TAVILY_API_KEY
 
 
-ALL_API = [GOOOGLE_API_KEY, GROQ_API_KEY, TAVILY_API_KEY]
+ALL_API = [GOOGLE_API_KEY, GROQ_API_KEY, TAVILY_API_KEY]
 
 if not all(ALL_API):
   st.sidebar.error("PASS API-KEYS")
 
 elif all(ALL_API):
-   # Step 1: Model Call
   model = ChatGoogleGenerativeAI(
       model = "gemini-3.5-flash-lite",
       google_api_key = GOOGLE_API_KEY
   )
-  st.sidebar.sucess("API KEYS LOADED SUCCESSFULLY")
+  st.sidebar.success("ALL KEYS LOADED SUCCESSFULLY")
 elif any(ALL_API):
-  st.sidebar.info("Must pass all API Keys")
+  st.sidebar.info("Must pass all API key")
 else:
-  st.sidebar.success("ALL Keys Entered")
+  st.sidebar.info("LOADED")
+  
   #==================FRONT END===========
   st.title("PPT-Generator-AI-Powered")
 
@@ -97,9 +97,7 @@ def prompt_generator(model, query):
   with open("ppt_prompt.txt",'w') as f:
     f.write(final_prompt)
   return final_prompt
-
 if all(ALL_API) and user_query:
-  
   agent = create_agent(
       model = model,
       tools = [search_latest_info,
@@ -112,16 +110,17 @@ if all(ALL_API) and user_query:
   #==============WITH TABS=================
   with tab1:
     st.header("GENERATE IMAGE GIVE PROMPT")
-    if st.button("Click to Generate: ", key="generate_img_button"):
-       with st.spinner("Running Agent.."):
+    if st.button("Click to Generate: ",key="generate_img_button"):
+      with st.spinner("Running Agent.."):
         data = f"https://image.pollinations.ai/{user_query}"
         time.sleep(3)
         st.image(data)
         #st.image(data)
         #st.image("Image.jpeg")
+  
   with tab2:
     st.header("CHECK LATEST NEWS")
-    if st.button("Fetch news: ", key="news_button"):
+    if st.button("Fetch news: ",key="news_button"):
       with st.spinner("Running Agent .. "):
       
         prompt = """Give latest news India or world wide news related
@@ -138,7 +137,7 @@ if all(ALL_API) and user_query:
   with tab3:
   
     st.header("Create PPT")
-    if st.button("Click to generate: ", key="generate_ppt_button"):
+    if st.button("Click to generate: ",key="generate_ppt_button"):
       with st.spinner("Running Agent .. "):
         final_prompt = prompt_generator(model, user_query)
         
@@ -148,9 +147,9 @@ if all(ALL_API) and user_query:
         code = response['messages' ] [-1] . content[-1] ['text' ]
         st.html(code, width="stretch",
         unsafe_allow_javascript=True)
-     if st.download_button(label = "DOWNLOAD PPT",
-                            data = code,
-                            file_name = 'ppt.html',
-                            mime = 'text/html'):
+        if st.download_button(label = "DOWNLOAD PPT",
+                data = code,
+                file_name = 'ppt.html',
+                mime = 'text/html'):
         
-           st.success("PPT Downloaded Successfully !! ")
+          st.success("PPT Downloaded Successfully !! ")
